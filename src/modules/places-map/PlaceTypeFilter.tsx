ffa93e66ed2b81b4c50e745b4fa86ac089488144
Konrad Iwan <kon.iwan17@gmail.com>
2023-02-17 17:46:28 +0100

    implementing mobile place type filter

--- src/modules/places-map/PlaceTypeFilter.tsx
+++ src/modules/places-map/PlaceTypeFilter.tsx
@@ -4,6 +4,7 @@ import { NextRouter, useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import { FiFilter } from "react-icons/fi";
 import { VscTriangleRight } from "react-icons/vsc";
+import { AiOutlineClose } from "react-icons/ai";
 import { usePlacesMapStore } from "../../zustand/placesMapStore";
 import PlaceTypeIcon from "../place/PlaceTypeIcon";
 
@@ -40,37 +41,47 @@ const PlaceTypeFilter = ({ placeTypes }: PlaceTypeFilterProps) => {
 
     // if (isLoading) return <LoadingSpinner />;
     return (
-        <div
-            onMouseLeave={() => setIsExpanded(false)}
-            onMouseEnter={() => setIsExpanded(true)}
-            className={clsx("relative block bg-primary shadow-md shadow-black transition-all hover:transition-all", { "w-64": isExpanded, "w-[72px]": !isExpanded })}
-        >
-            <div className={clsx("flex h-12 items-center")}>
-                <FiFilter className="ml-5 shrink-0 text-3xl" />
-                {isExpanded && <p className="ml-5 overflow-hidden whitespace-nowrap font-bold">Filtruj typ miejsca</p>}
+        <>
+            <div className={clsx("absolute bottom-2 left-2 z-[999]")}>
+                <button className={clsx("w-full rounded-full bg-dark p-4 shadow-sm shadow-indigo-600 transition-all small:hidden", { "w-60 rounded-sm ": isExpanded })} onClick={() => setIsExpanded(prev => !prev)}>
+                    {isExpanded ? (
+                        <div className="flex items-center justify-center gap-2">
+                            <AiOutlineClose className="text-2xl text-secondary" />
+                            Zamknij
+                        </div>
+                    ) : (
+                        <FiFilter className="text-5xl text-secondary" />
+                    )}
+                </button>
             </div>
-            <div
-                className={clsx("absolute -left-1 flex h-16 w-1 translate-y-12 flex-col justify-center rounded-r-md bg-indigo-600  transition-all", { hidden: indicatorPosition === null })}
-                style={{ top: `${(indicatorPosition ? indicatorPosition : 0) * 4}rem` }}
-            >
-                <VscTriangleRight className="-translate-x-[4px] text-xl text-indigo-600" />
+            <div className={clsx("relative block shrink-0 bg-primary shadow-md shadow-black transition-all hover:w-64 hover:transition-all", { "w-64": isExpanded, "hidden w-[72px] small:block": !isExpanded })}>
+                <div className={clsx("flex h-12 items-center overflow-hidden")}>
+                    <FiFilter className="ml-5 shrink-0 text-3xl" />
+                    <p className="ml-6 overflow-hidden whitespace-nowrap font-bold">Filtruj typ miejsca</p>
+                </div>
+                <div
+                    className={clsx("absolute -left-1 flex h-16 w-1 translate-y-12 flex-col justify-center rounded-r-md bg-indigo-600  transition-all", { hidden: indicatorPosition === null })}
+                    style={{ top: `${(indicatorPosition ? indicatorPosition : 0) * 4}rem` }}
+                >
+                    <VscTriangleRight className="-translate-x-[4px] text-xl text-indigo-600" />
+                </div>
+                <ul className="flex flex-col rounded-t-md text-center text-sm font-medium text-gray-500 dark:text-gray-400">
+                    {placeTypes?.map((place, index) => (
+                        <li
+                            key={place.id}
+                            className={clsx("py-1 px-2", {
+                                "bg-dark": selectedTypeId === place.id
+                            })}
+                        >
+                            <button className={clsx("flex items-center gap-2 hover:border-indigo-400 hover:text-gray-600 dark:hover:text-gray-300")} onClick={() => handleSelectPlaceType(place.id, index)}>
+                                <PlaceTypeIcon size="md" placeType={place} />
+                                <span className="whitespace-nowrap text-base">{place.title}</span>
+                            </button>
+                        </li>
+                    ))}
+                </ul>
             </div>
-            <ul className="flex flex-col rounded-t-md text-center text-sm font-medium text-gray-500 dark:text-gray-400">
-                {placeTypes?.map((place, index) => (
-                    <li
-                        key={place.id}
-                        className={clsx("py-1 px-2", {
-                            "bg-dark": selectedTypeId === place.id
-                        })}
-                    >
-                        <button className={clsx("flex items-center gap-2 hover:border-indigo-400 hover:text-gray-600 dark:hover:text-gray-300")} onClick={() => handleSelectPlaceType(place.id, index)}>
-                            <PlaceTypeIcon size="md" placeType={place} />
-                            {isExpanded && <span className="whitespace-nowrap text-base">{place.title}</span>}
-                        </button>
-                    </li>
-                ))}
-            </ul>
-        </div>
+        </>
     );
 };
 
