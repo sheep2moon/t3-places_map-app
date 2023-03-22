9364e568ec7bcb76d18b1907a4b57daf08a182dd
sheep2moon <kon.iwan17@gmail.com>
2023-03-22 10:47:53 +0100

    adding notification model to prisma schema

--- src/modules/common/badges/UserBadge.tsx
+++ src/modules/common/badges/UserBadge.tsx
@@ -3,16 +3,21 @@ import Image from "next/image";
 import React from "react";
 
 const UserBadge = ({ user }: { user: User }) => {
-    return (
-        <div className="flex items-center space-x-2 rounded-sm p-1 text-xs">
-            <div className="relative aspect-square h-4">
-                <Image src={user.image ?? ""} className="rounded-sm" alt="awatar użytkownika" layout="fill" />
-            </div>
-            <div className="text-base font-medium dark:text-white">
-                <div>{user.name}</div>
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex items-center space-x-2 rounded-sm p-1 text-xs">
+      <div className="relative aspect-square h-4">
+        <Image
+          src={user.image ?? ""}
+          className="rounded-sm"
+          alt="awatar użytkownika"
+          fill
+        />
+      </div>
+      <div className="text-base font-medium dark:text-white">
+        <div>{user.name}</div>
+      </div>
+    </div>
+  );
 };
 
 export default UserBadge;
--- src/modules/layout/nav/components/MobileMenu.tsx
+++ src/modules/layout/nav/components/MobileMenu.tsx
@@ -5,35 +5,56 @@ import { NavLink } from "..";
 import ThemeSwitch from "./ThemeSwitch";
 
 type MobileMenuProps = {
-    navLinks: NavLink[];
-    close: () => void;
-    isOpen: boolean;
+  navLinks: NavLink[];
+  close: () => void;
+  isOpen: boolean;
+  isLoggedIn: boolean;
 };
 
-const MobileMenu = ({ navLinks, isOpen, close }: MobileMenuProps) => {
-    return (
-        <div
-            className={clsx("fixed inset-x-0 top-16 bottom-0 z-50 bg-gradient-to-br from-light to-secondary p-2 text-dark transition dark:from-primary dark:to-dark dark:text-secondary", {
-                "translate-x-0": isOpen,
-                "translate-x-full": !isOpen
+const MobileMenu = ({
+  navLinks,
+  isOpen,
+  close,
+  isLoggedIn,
+}: MobileMenuProps) => {
+  return (
+    <div
+      className={clsx(
+        "fixed inset-x-0 top-16 bottom-0 z-50 bg-gradient-to-br from-light to-secondary p-2 text-dark transition dark:from-primary dark:to-dark dark:text-secondary",
+        {
+          "translate-x-0": isOpen,
+          "translate-x-full": !isOpen,
+        }
+      )}
+      onClick={(e) => e.stopPropagation()}
+    >
+      <div className="flex h-full flex-col items-center justify-center gap-4">
+        <div className="flex flex-col gap-8">
+          <div className="grid gap-4">
+            {navLinks.map((link) => {
+              if ((isLoggedIn && link.needAuth) || !link.needAuth) {
+                return (
+                  <Link
+                    href={link.href}
+                    key={link.title}
+                    onClick={close}
+                    className="flex items-center gap-2 text-3xl"
+                  >
+                    <span className="text-indigo-800 dark:text-indigo-400">
+                      {link.icon}
+                    </span>
+                    <span>{link.title}</span>
+                  </Link>
+                );
+              }
+              return null;
             })}
-            onClick={e => e.stopPropagation()}
-        >
-            <div className="flex h-full flex-col items-center justify-center gap-4">
-                <div className="flex flex-col gap-8">
-                    <div className="grid gap-4">
-                        {navLinks.map(link => (
-                            <Link href={link.href} key={link.title} onClick={close} className="flex items-center gap-2 text-3xl">
-                                <span className="text-indigo-800 dark:text-indigo-400">{link.icon}</span>
-                                <span>{link.title}</span>
-                            </Link>
-                        ))}
-                    </div>
-                    <ThemeSwitch />
-                </div>
-            </div>
+          </div>
+          <ThemeSwitch />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default MobileMenu;
