48fc84265ee1b73e374e56bdad38bca691a143ea
sheep2moon <kon.iwan17@gmail.com>
2023-03-29 15:37:14 +0200

    implementing tabs in user profile page

--- src/pages/user/profile.tsx
+++ src/pages/user/profile.tsx
@@ -1,53 +1,88 @@
 import { Tab } from "@headlessui/react";
 import clsx from "clsx";
-import React from "react";
+import React, { Fragment } from "react";
+import Button from "../../modules/common/Button";
 import InternalLink from "../../modules/common/links/InternalLink";
+import LoadingSpinner from "../../modules/common/LoadingSpinner";
+import { signOut, useSession } from "next-auth/react";
+import { trpc } from "../../utils/trpc";
+import UserAvatar from "../../modules/common/UserAvatar";
+import { MdFavorite } from "react-icons/md";
+import { useRouter } from "next/router";
+import { usePlacesMapStore } from "../../zustand/placesMapStore";
+import { Place, PlaceType } from "@prisma/client";
+import PlaceTypeIcon from "../../modules/place/PlaceTypeIcon";
+import { BiShowAlt } from "react-icons/bi";
+import { IoWalk } from "react-icons/io5";
+import UserPlace from "../../modules/user-places/UserPlace";
+import Link from "next/link";
+import { FaArrowRight } from "react-icons/fa";
 
 const tabList = [
   {
     id: 1,
-    title: "Biblioteka",
+    title: "Ulubione",
+    icon: <MdFavorite className=" text-secondary" />,
   },
   {
     id: 2,
-    title: "Moje miejsca",
+    title: "Odwiedzone",
+    icon: <IoWalk className=" text-secondary" />,
+  },
+  {
+    id: 3,
+    title: "Dodane",
   },
 ];
 
 const UserProfile = () => {
+  const { data, isLoading } = trpc.useQuery(["user.getUserProfileData"]);
+
+  if (!data || isLoading)
+    return (
+      <div className="h-container-screen relative w-full">
+        <LoadingSpinner />
+      </div>
+    );
+
   return (
     <div className="flex w-full max-w-screen-xl flex-col">
-      <section className="flex w-full flex-col items-center px-4 py-12 dark:bg-dark sm:py-16 sm:px-6 lg:px-8 lg:py-20">
-        <div className="mx-auto max-w-4xl text-center">
+      <section className="flex w-full flex-col items-center px-4 py-4 dark:bg-dark">
+        <Button className="ml-auto" variant="outline" onClick={() => signOut()}>
+          Wyloguj się
+        </Button>
+        <div className="mx-auto flex max-w-4xl flex-col items-center text-center">
+          {data.image && <UserAvatar image={data.image} />}
           <h2 className="text-3xl font-extrabold leading-9 text-white sm:text-4xl sm:leading-10">
-            Used by leading architects, home builders renovators.
+            {data.name}
           </h2>
           <p className="mt-3 text-base leading-7 text-white sm:mt-4">
-            Feel confident in choosing the best energy assessor for your energy
-            rating.
+            {data.email}
           </p>
         </div>
-        <div className="mt-10 text-center sm:mx-auto sm:grid sm:max-w-3xl sm:grid-cols-3 sm:gap-8">
+        <div className="mt-4 text-center sm:mx-auto sm:grid sm:max-w-3xl sm:grid-cols-3 sm:gap-8">
           <div>
             <p className="text-5xl font-extrabold leading-none text-white">
-              119
+              {data.UserLibrary?.visited.length}
             </p>
             <p className="mt-2 text-base font-medium leading-6 text-white">
-              Energy raters
-            </p>
-          </div>
-          <div className="mt-10 sm:mt-0">
-            <p className="text-5xl font-extrabold leading-none text-white">6</p>
-            <p className="mt-2 text-base font-medium leading-6 text-white">
-              Quotes on average
+              Odwiedzonych miejsc
             </p>
           </div>
           <div className="mt-10 sm:mt-0">
             <p className="text-5xl font-extrabold leading-none text-white">
-              24 hours
+              {data.places.length}
             </p>
             <p className="mt-2 text-base font-medium leading-6 text-white">
-              Average turnaround
+              Dodanych miejsc
+            </p>
+          </div>
+          <div className="mt-10 sm:mt-0">
+            <p className="text-5xl font-extrabold leading-none text-white">
+              {data.reviews.length}
+            </p>
+            <p className="mt-2 text-base font-medium leading-6 text-white">
+              Dodanych recenzji
             </p>
           </div>
         </div>
@@ -55,31 +90,81 @@ const UserProfile = () => {
           Dodaj miejsce
         </InternalLink>
       </section>
-      <Tab.Group>
-        <Tab.List className="flex">
+      <Tab.Group as="div" className="mt-4">
+        <Tab.List className="flex bg-light/5">
           {tabList.map((tab) => (
             <Tab
               key={tab.id}
               className={({ selected }) =>
                 clsx(
-                  "w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-blue-700",
+                  "flex w-full items-center justify-center gap-2 rounded-lg py-2.5 text-xl text-sm font-medium leading-5 ",
                   "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2",
                   {
-                    "bg-white shadow": selected,
-                    "text-blue-100 hover:bg-white/[0.12] hover:text-white":
+                    "bg-indigo-400 font-bold text-indigo-900 shadow": selected,
+                    "text-light hover:bg-white/[0.12] hover:text-white":
                       !selected,
                   }
                 )
               }
             >
+              {tab.icon}
               {tab.title}
             </Tab>
           ))}
         </Tab.List>
         <Tab.Panels>
-          <Tab.Panel>Content 1</Tab.Panel>
-          <Tab.Panel>Content 2</Tab.Panel>
-          <Tab.Panel>Content 3</Tab.Panel>
+          <Tab.Panel>
+            <div className="mt-2">
+              {data.UserLibrary?.wishlist.length === 0 && (
+                <span className="ml-4 text-xs">Brak ulubionych miejsc</span>
+              )}
+              {data.UserLibrary?.wishlist.map((place) => (
+                <LibraryPlace key={place.id} place={place} />
+              ))}
+            </div>
+          </Tab.Panel>
+          <Tab.Panel>
+            <div className="mt-2">
+              {data.UserLibrary?.visited.length === 0 && (
+                <h4 className="mt-8 text-center text-lg">
+                  Brak miejsc na liście do odwiedzenia
+                </h4>
+              )}
+              {data.UserLibrary?.visited.map((place) => (
+                <LibraryPlace key={place.id} place={place} />
+              ))}
+            </div>
+          </Tab.Panel>
+          <Tab.Panel>
+            <div className="flex flex-col gap-2 py-2">
+              {data.places.map((place) => (
+                <div
+                  key={place.id}
+                  className="flex w-full items-center rounded-sm bg-black/20 py-2 px-2 text-xl"
+                >
+                  <PlaceTypeIcon placeType={place.type} size="sm" />
+                  <div className="ml-4">{place.displayName}</div>
+                  <Link
+                    href={`/user-places/${place.id}`}
+                    className="ml-auto px-3"
+                  >
+                    <FaArrowRight />
+                  </Link>
+                </div>
+              ))}
+              {data.places.length === 0 && (
+                <div className="flex items-center gap-1 py-1 text-xl">
+                  <span>Nie dodałeś jeszcze żadnych miejsc</span>
+                  <Link
+                    href="/add-place"
+                    className="rounded-sm bg-amber-200 p-1 text-xl text-primary"
+                  >
+                    Dodaj miejsce
+                  </Link>
+                </div>
+              )}
+            </div>
+          </Tab.Panel>
         </Tab.Panels>
       </Tab.Group>
     </div>
@@ -87,3 +172,32 @@ const UserProfile = () => {
 };
 
 export default UserProfile;
+
+const LibraryPlace = ({ place }: { place: Place & { type: PlaceType } }) => {
+  const { setCurrentPlaceId, setIsPlaceModalOpen } = usePlacesMapStore(
+    (state) => state
+  );
+  const router = useRouter();
+
+  const handleGoToPlace = () => {
+    setCurrentPlaceId(place.id);
+    setIsPlaceModalOpen(true);
+    router.push("/places-map");
+  };
+
+  return (
+    <div className="flex items-center gap-2 rounded-sm py-1 pr-1 ">
+      <div>
+        <PlaceTypeIcon size="sm" placeType={place.type} />
+      </div>
+      <span className="text-xl">{place.displayName}</span>
+      <button
+        onClick={handleGoToPlace}
+        className="ml-auto flex items-center gap-2 rounded-sm p-2 hover:bg-primary/5 dark:text-amber-200 dark:hover:bg-light/5"
+      >
+        <span className="text-base dark:text-amber-100">Mapa</span>
+        <BiShowAlt />
+      </button>
+    </div>
+  );
+};
--- src/server/router/user.ts
+++ src/server/router/user.ts
@@ -3,97 +3,122 @@ import { env } from "../../env/server.mjs";
 import { createProtectedRouter } from "./context";
 
 export const userRouter = createProtectedRouter()
-    .query("getUserDetails", {
-        async resolve({ ctx }) {
-            return await ctx.prisma.user.findFirst({
-                where: {
-                    id: ctx.session.user.id
-                },
-                include: {
-                    reviews: true
-                }
-            });
-        }
-    })
-    .query("getUserReviewByPlaceId", {
-        input: z.object({
-            placeId: z.string()
-        }),
-        async resolve({ input, ctx }) {
-            return await prisma?.review.findFirst({
-                where: {
-                    placeId: input.placeId,
-                    createdBy: ctx.session.user.id
-                }
-            });
-        }
-    })
-    .query("getUserLibrary", {
-        async resolve({ ctx }) {
-            return await prisma?.userLibrary.upsert({ where: { userId: ctx.session.user.id }, update: {}, create: { userId: ctx.session.user.id }, include: { wishlist: { include: { type: true } }, visited: { include: { type: true } } } });
-        }
-    })
-    .mutation("toggleWishlistPlace", {
-        input: z.object({
-            placeId: z.string()
-        }),
-        async resolve({ input, ctx }) {
-            const isWishlisted = await ctx.prisma.userLibrary.findFirst({
-                where: {
-                    userId: ctx.session.user.id,
-                    wishlist: { some: { id: input.placeId } }
-                }
-            });
-            if (isWishlisted) {
-                await ctx.prisma.userLibrary.update({
-                    where: { userId: ctx.session.user.id },
-                    data: {
-                        wishlist: {
-                            disconnect: { id: input.placeId }
-                        }
-                    }
-                });
-            } else {
-                await ctx.prisma.userLibrary.update({
-                    where: { userId: ctx.session.user.id },
-                    data: {
-                        wishlist: {
-                            connect: { id: input.placeId }
-                        }
-                    }
-                });
-            }
-        }
-    })
-    .mutation("toggleVisitedPlace", {
-        input: z.object({
-            placeId: z.string()
-        }),
-        async resolve({ input, ctx }) {
-            const isVisited = await ctx.prisma.userLibrary.findFirst({
-                where: {
-                    userId: ctx.session.user.id,
-                    visited: { some: { id: input.placeId } }
-                }
-            });
-            if (isVisited) {
-                await ctx.prisma.userLibrary.update({
-                    where: { userId: ctx.session.user.id },
-                    data: {
-                        visited: {
-                            disconnect: { id: input.placeId }
-                        }
-                    }
-                });
-            } else {
-                await ctx.prisma.userLibrary.update({
-                    where: { userId: ctx.session.user.id },
-                    data: {
-                        visited: {
-                            connect: { id: input.placeId }
-                        }
-                    }
-                });
-            }
-        }
-    });
+  .query("getUserDetails", {
+    async resolve({ ctx }) {
+      return await ctx.prisma.user.findFirst({
+        where: {
+          id: ctx.session.user.id,
+        },
+        include: {
+          reviews: true,
+        },
+      });
+    },
+  })
+  .query("getUserReviewByPlaceId", {
+    input: z.object({
+      placeId: z.string(),
+    }),
+    async resolve({ input, ctx }) {
+      return await prisma?.review.findFirst({
+        where: {
+          placeId: input.placeId,
+          createdBy: ctx.session.user.id,
+        },
+      });
+    },
+  })
+  .query("getUserLibrary", {
+    async resolve({ ctx }) {
+      return await prisma?.userLibrary.upsert({
+        where: { userId: ctx.session.user.id },
+        update: {},
+        create: { userId: ctx.session.user.id },
+        include: {
+          wishlist: { include: { type: true } },
+          visited: { include: { type: true } },
+        },
+      });
+    },
+  })
+  .query("getUserProfileData", {
+    async resolve({ ctx }) {
+      return await prisma?.user.findFirst({
+        where: { id: ctx.session.user.id },
+        include: {
+          UserLibrary: {
+            include: {
+              visited: { include: { type: true } },
+              wishlist: { include: { type: true } },
+            },
+          },
+          places: { include: { type: true } },
+          reviews: true,
+        },
+      });
+    },
+  })
+  .mutation("toggleWishlistPlace", {
+    input: z.object({
+      placeId: z.string(),
+    }),
+    async resolve({ input, ctx }) {
+      const isWishlisted = await ctx.prisma.userLibrary.findFirst({
+        where: {
+          userId: ctx.session.user.id,
+          wishlist: { some: { id: input.placeId } },
+        },
+      });
+      if (isWishlisted) {
+        await ctx.prisma.userLibrary.update({
+          where: { userId: ctx.session.user.id },
+          data: {
+            wishlist: {
+              disconnect: { id: input.placeId },
+            },
+          },
+        });
+      } else {
+        await ctx.prisma.userLibrary.update({
+          where: { userId: ctx.session.user.id },
+          data: {
+            wishlist: {
+              connect: { id: input.placeId },
+            },
+          },
+        });
+      }
+    },
+  })
+  .mutation("toggleVisitedPlace", {
+    input: z.object({
+      placeId: z.string(),
+    }),
+    async resolve({ input, ctx }) {
+      const isVisited = await ctx.prisma.userLibrary.findFirst({
+        where: {
+          userId: ctx.session.user.id,
+          visited: { some: { id: input.placeId } },
+        },
+      });
+      if (isVisited) {
+        await ctx.prisma.userLibrary.update({
+          where: { userId: ctx.session.user.id },
+          data: {
+            visited: {
+              disconnect: { id: input.placeId },
+            },
+          },
+        });
+      } else {
+        await ctx.prisma.userLibrary.update({
+          where: { userId: ctx.session.user.id },
+          data: {
+            visited: {
+              connect: { id: input.placeId },
+            },
+          },
+        });
+      }
+    },
+  });
