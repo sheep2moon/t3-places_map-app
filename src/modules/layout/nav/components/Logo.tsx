add415b021e1fc86da2074a9b8d4bf1bb383329d
Konrad Iwan <kon.iwan17@gmail.com>
2023-04-12 22:31:02 +0200

    replacing text logo with svg logo

--- src/modules/layout/nav/components/Logo.tsx
+++ src/modules/layout/nav/components/Logo.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import React from "react";
 import { Rubik_Vinyl } from "@next/font/google";
+import Image from "next/image";
+import logoSrc from "../../../../assets/plecaklogo.svg";
 
 const rubikVinyl = Rubik_Vinyl({ weight: "400", subsets: ["latin"] });
 
@@ -9,7 +11,8 @@ const Logo = () => {
         <div>
             <Link className="mr-8 flex items-center gap-1" href="/">
                 {/* <div className="relative h-10 w-10"><Image src={logoSrc} alt="logo strony" objectFit="contain" layout="fill" /></div> */}
-                <p className={"font-pinstripe text-2xl font-normal text-secondary sm:text-3xl " + rubikVinyl.className}>PLECAK</p>
+                {/* <p className={"font-pinstripe text-2xl font-normal text-secondary sm:text-3xl " + rubikVinyl.className}>PLECAK</p> */}
+                <Image src={logoSrc} alt="logo aplikacji" className="w-36" />
             </Link>
         </div>
     );
